b9e2e028b098756dc32977812324da21ffed2459
Maria <mapis2321@gmail.com>
2023-11-01 17:57:50 -0500

    Se tomo el archivo csv como un objeto Json

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import model.Product;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+
 import java.util.Scanner;
 
 public class Main {
@@ -12,9 +17,16 @@ public class Main {
             while (fileScanner.hasNextLine()) {
                 String line = fileScanner.nextLine();
                 String[] productInfo = line.split(",");
-                System.out.println(productInfo[0]);
-                System.out.println(line);
+                
+                //System.out.println(productInfo[0]);
+                //System.out.println(line);
                 //TODO implement the logic to transform this code so it can convert the information in each line into a Product objet
+                
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                String JSON = gson.toJson(productInfo);
+                //String x = gson.toJson(line);
+                //System.out.println(x);
+                System.out.println(JSON);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
--- src/model/Product.java
+++ src/model/Product.java
@@ -24,6 +24,8 @@ public class Product {
         this.imageUrl = imageUrl;
     }
 
+    public Product() {}
+
     public String getName() {
         return name;
     }
